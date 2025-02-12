97dfcc3f6cfbdece2298bfa12fe0a3ef9657112f
zheka365 <zheka365@yandex.ru>
2025-02-12 23:20:34 +0300

    123

--- src/components/apiBook.js
+++
@@ -1,3 +0,0 @@
-// https://www.googleapis.com/books/v1/volumes?q="subject:Business"&key=<ваш ключ API>&printType=books&startIndex=0&maxResults=6&langRestrict=en
-const ApiKey = "AIzaSyAersGrpFzMRiU9ntCg4mdgDGPQFrULqOI";
-const ApiUrl = `https://www.googleapis.com/books/v1/volumes`;
--- src/components/params.js
+++
@@ -1,9 +0,0 @@
-const params = new URLSearchParams({});
-let startIndex = 0;
-params.append("key", "AIzaSyAersGrpFzMRiU9ntCg4mdgDGPQFrULqOI");
-params.append("printType", "books");
-params.append("startIndex", startIndex);
-params.append("maxResults", "6");
-params.append("langRestrict", "en");
-
-export default params;
